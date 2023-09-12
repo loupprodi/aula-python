c73ac6906790afca0f65a50adf579f32c80ac35a
LUIZ FELIPE VICENTE PROSDOSKIMI <00098365@unisoacad.uniso.br>
2023-09-12 20:16:15 -0300

    primeiro commit

---
+++ index.py
@@ -0,0 +1,15 @@
+def soma(a,b):
+
+    return a + b
+
+
+
+print("hello world")
+
+a = 20
+
+b = int(input("Digite um número"))
+
+
+
+print(soma(a,b))
---
+++ matrizes.py
@@ -0,0 +1,19 @@
+# nota = [6, 95, 80, 50, 98]
+
+# print(nota[3])
+
+# x = nota * 2
+
+# print(x)
+
+# luiz = "Luiz Prosdoskimi"
+
+# for i, t in enumerate(luiz):
+#     print(i, t)
+
+
+times = ["são paulo", "corinthians", "palmeiras"]
+nomes = ["Luiz", "Raul","Eduardo"]
+
+for i in zip(times, nomes):
+    print(i)
